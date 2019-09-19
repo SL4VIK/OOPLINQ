ae6864672e7ba7667c4dadfc2948b12f5facc251
Iamelvira <elyakraso4naja@gmail.com>
2019-09-19 21:11:03 +0300

    1

--- OOPLINQ/Program.cs
+++ OOPLINQ/Program.cs
@@ -10,6 +10,17 @@ namespace OOPLINQ
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(string.Join(", ", Enumerable.Range(10, 41)));
+            //Console.WriteLine(string.Join(", ", Enumerable.Range(10, 41).Where((i) => i % 3 == 0)));
+            //Console.WriteLine(string.Concat(Enumerable.Repeat("LINQ", 10)));
+            //Console.WriteLine(string.Join(", ", "aaa;abb;ccc;dap".Split(';').Where(str => str.Contains('a'))));
+            //Console.WriteLine(string.Join(", ", "aaa;abb;ccc;dap".Split(';').Where(str => str.Contains('a')).Select(str => str.Count(letter => letter == 'a'))));
+            //Console.WriteLine(string.Join(", ", "aaa;xabbx;abb;ccc;dap".Split(';').Select(str => str.Contains("abb"))));
+            Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Max());
+
+
+
+            Console.ReadKey();
         }
     }
 }
